1ab9f726cc6adaf6712fe71b426346f1c8f192c4
kales5078 <88039478+kales5078@users.noreply.github.com>
2021-08-17 02:01:32 +0530

    Add files via upload

--- EmployeeAttendance.java
+++ EmployeeAttendance.java
@@ -1,20 +1,34 @@
 package company;
 
-import java.lang.Math;  //Import Math function to get Random Number
-public class EmployeeAttendance
-{
+import java.lang.Math; //Import Math function to get Random Number
+
+public class EmployeeAttendance {
 	public static void main(String[] args) {
-	 
-	 int IS_PRESENT = 1;  
-	 double Empcheck = Math.floor(Math.random() * 10) % 2;
-	 
-	 
-	   if (Empcheck == IS_PRESENT)
-	    System.out.println("Employee is Present");
-	    else
-	     System.out.println("Employee is Absent");
-	 
-	   
-	   
+
+		int IS_ABSENT = 0;
+		int Wage_Per_Hour = 20;
+		int IS_PRESENT_FULL_TIME_WAGE = Wage_Per_Hour * 8;
+		int IS_PRESENT_HALF_TIME_WAGE = Wage_Per_Hour * 4;
+		double Empcheck = Math.floor(Math.random() * 10) % 3;
+
+		int value = (int) Empcheck; // TO convert double value into intiger
+									// value
+		switch (value) {
+
+		case 1:
+			System.out
+					.println("Employee is present for full time and wage is Rs:"
+							+ IS_PRESENT_FULL_TIME_WAGE);
+			break;
+		case 2:
+			System.out
+					.println("Employee is present for Half time and wage is Rs:"
+							+ IS_PRESENT_HALF_TIME_WAGE);
+			break;
+		default:
+			System.out
+					.println("Employee is absent and wage is Rs:" + IS_ABSENT);
+		}
+
 	}
 }
