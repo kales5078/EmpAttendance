1f7e5d224633a723818afc0276075d96c6b36e24
kales5078 <88039478+kales5078@users.noreply.github.com>
2021-08-18 21:57:56 +0530

    Modified : modified the comment

--- GetLength.java
+++ GetLength.java
@@ -6,7 +6,7 @@ public class GetLength
 	       System.out.println("This is program for calculate length");
 		Scanner sc = new Scanner(System.in); 
 		System.out.println("Enter the value of x1");
-		double x1 = sc.nextDouble(); 
+		double x1 = the lenh sc.nextDouble(); 
 		System.out.println("Enter the value of x2");
 		double x2 = sc.nextDouble();
 	        System.out.println("Enter the value of y1");
@@ -17,5 +17,6 @@ public class GetLength
        
        length = Math.sqrt((x2 - x1 ) + (y2 - y1) * (x2 - x1 ) + (y2 - y1));
        System.out.println("The length is :" + length);
+		//this is the program for get
 	}
 }
